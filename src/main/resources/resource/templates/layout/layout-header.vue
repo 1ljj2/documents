f79b4aca3b33701eb8893fd9e3ff5fc0fb116dd9
1ljj2 <2994125780@qq.com>
2025-06-13 17:21:40 +0800

    6.13更新

--- src/main/resources/resource/templates/layout/layout-header.vue
+++ src/main/resources/resource/templates/layout/layout-header.vue
@@ -7,14 +7,14 @@
 					<div class="layout-logo">&nbsp;&nbsp;<span>{{projectName}}</span></div>
 					<div class="layout-nav">
 
-						<span>欢迎您</span>
-						<a id="user_name" href="javascript:void(0);">{{userName}}</a>
+						<span style="font-size:16px;">欢迎您</span>
+						<a style="font-size:16px;" id="user_name" href="javascript:void(0);">{{userName}}</a>
 						<!--<span>未读消息<a id="todo_affair_count" @click="">{{todoAffairCount}}10</a>条</span>-->
 						<!--<el-badge :value="10" :max="99" class="item">
 						  <el-button size="mini" type="danger" plain @click="jumpTodoAffair('/affair/todoAffair.html')">待办事务</el-button>
 						</el-badge>-->
-						<span class="nowTime">{{realTime}}</span>
-						<span><a @click=quitLogin()>退出</a></span>
+						<span style="font-size:16px;" class="nowTime">{{realTime}}</span>
+						<span><a style="font-size:16px;" @click=quitLogin()>退出</a></span>
 					</div>
 				</i-menu>
 			</i-header>
